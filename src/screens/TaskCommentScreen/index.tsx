00e984a04f4cfafa1b93a078e9d789e42d36a060
Virtuso1225 <dominico1225@gmail.com>
2024-03-15 23:09:36 +0900

    ftr: CL-PG-13 router linking

---
+++ src/screens/TaskCommentScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text } from 'tamagui'
+
+import { SafeArea } from '@/components/SafeArea'
+
+const TaskCommentScreen = () => {
+  return (
+    <SafeArea>
+      <Text>TaskCommentScreen</Text>
+    </SafeArea>
+  )
+}
+
+export default TaskCommentScreen
